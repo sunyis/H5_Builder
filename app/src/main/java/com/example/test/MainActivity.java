70a0d8cdbdcc8af2151d739b7e6a24bd8eeaf596
sunyis <572383342@qq.com>
2024-10-26 18:45:42 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -102,11 +102,10 @@ public class MainActivity extends AppCompatActivity {
                 
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         
-                File customDir = new File(context.getExternalFilesDir(null), "MyCustomDownloads");
-        if (!customDir.exists()) {
-            customDir.mkdirs(); // 创建目录
-        }
-                request.setDestinationUri(Uri.fromFile(new File(downloadDir, URLUtil.guessFileName(url, contentDisposition, mimetype))));      
+                File downloadDir = new File(Environment.getExternalStorageDirectory(), "电视直播");
+                if (!downloadDir.exists()) {
+                downloadDir.mkdirs(); 
+                }
 
                 DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
