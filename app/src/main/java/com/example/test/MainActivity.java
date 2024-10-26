973ce3e8e2c890f149704023a8d6dd86fd3f3f6d
sunyis <572383342@qq.com>
2024-10-26 17:26:19 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         
-                File downloadDir = new File("/", "电视直播");
+                File downloadDir = new File("电视直播");
                 if (!downloadDir.exists()) {
                 downloadDir.mkdirs();
 }
